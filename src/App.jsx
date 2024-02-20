8659f2462e421fcb12abe3bc2dd4fbb1f79d4983
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-02-20 19:29:27 +0500

    remove un-necsessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useContext, useEffect, useState } from "react";
 import Stack from "@mui/material/Stack";
 import { Button } from "@mui/material";
-
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import ForgotPassword from "./pages/auth/ForgotPassword";
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-  useNavigate,
-} from "react-router-dom";
-import { AuthContext, AuthContextProvider } from "./context/AuthContext";
-import { auth } from "../firebase.config";
-import { getAuth } from "firebase/auth";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { MainLayout } from "./components/mainLayout/MainLayout";
 
 function App() {
